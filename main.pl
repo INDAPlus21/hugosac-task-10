965dbc6a35207ccd44052a5f1c991054bd7840d0
Hugo <hugosac@kth.se>
2021-11-25 21:04:17 +0100

    Should work

--- main.pl
+++ main.pl
@@ -7,7 +7,7 @@ nth1_2d(Row, Column, List, Element) :-
 
 
 % Top-level predicate
-alive(Row, Column, BoardFileName):-
+alive(Row, Column, BoardFileName) :-
     see(BoardFileName),     % Loads the input-file
     read(Board),            % Reads the first Prolog-term from the file
     seen,                   % Closes the io-stream
@@ -16,13 +16,13 @@ alive(Row, Column, BoardFileName):-
 
 % Checks whether the group of stones connected to
 % the stone located at (Row, Column) is alive or dead.
-check_alive(Row, Column, Board):-
+check_alive(Row, Column, Board) :-
     % Check that there is a stone in the given position.
     nth1_2d(Row, Column, Board, Stone),
     (Stone = b; Stone = w),
     check_empty(Row, Column, Board, Stone, []).
 
-% Check if a coordinate has already been visited
+% Check if a coordinate has already been checked
 % by checking if a list contains the coordinate.
 already_checked(Coord, [H | T]) :-
     Coord = H ; already_checked(Coord, T).
@@ -34,12 +34,12 @@ check_empty(Row, Column, Board, Stone, CheckedList) :-
     % Get the stone color from the coordinate of the current stone.
     nth1_2d(Row, Column, Board, CurrentStone),
 
-    % The current position is empty
-    (CurrentStone = e ; 
+    % If current position is empty, return true.
+    (CurrentStone = e ;
         % Current stone has the same color as the group to search through.
         (CurrentStone = Stone, 
-            % Coordinate has not been visited already.
-            \+ already_checked((Row, Column), CheckedList), 
+            % Coordinate has not been checked already.
+            \+ already_checked([Row, Column], CheckedList), 
 
             % All four directions
             RowUp is Row + 1,
@@ -49,10 +49,10 @@ check_empty(Row, Column, Board, Stone, CheckedList) :-
             (
                 % Recursively check adjacent positions. Append the checked postions to
                 % the CheckedList.
-                check_empty(RowUp, Column, Board, Stone, [(Row, Column) | CheckedList]);
-                check_empty(RowDown, Column, Board, Stone, [(Row, Column) | CheckedList]);
-                check_empty(Row, ColLeft, Board, Stone, [(Row, Column) | CheckedList]);
-                check_empty(Row, ColRight, Board, Stone, [(Row, Column) | CheckedList])
+                check_empty(RowUp, Column, Board, Stone, [[Row, Column] | CheckedList]);
+                check_empty(RowDown, Column, Board, Stone, [[Row, Column] | CheckedList]);
+                check_empty(Row, ColLeft, Board, Stone, [[Row, Column] | CheckedList]);
+                check_empty(Row, ColRight, Board, Stone, [[Row, Column] | CheckedList])
             )
         )
     ).
